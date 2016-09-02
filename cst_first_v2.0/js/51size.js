19ba9f7a395e0ae68a4cb6957dfd47157b415a49
Fangzhu-Tianji <zch793262201@163.com>
2016-09-02 17:22:09 +0800

    add v2.0
    添加移动端的v2.0

---
+++ cst_first_v2.0/js/51size.js
@@ -0,0 +1,15 @@
+﻿(function (doc, win) {
+    var docEl = doc.documentElement,
+      resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+      recalc = function () {
+          var clientWidth = docEl.clientWidth;
+          if (!clientWidth) return;
+          docEl.style.fontSize = 100 * (clientWidth / 750) + 'px';
+          //这里的clientWidth是视口的宽度，被除数是可以调整的，根据设计稿而定
+          //如果是iPhone6，则可写成：
+          //docEl.style.fontSize = 100 * (clientWidth / 375) + 'px';
+      };
+    if (!doc.addEventListener) return;
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
---
+++ cst_first_v2.0/js/common.js
@@ -0,0 +1,24 @@
+$(function(){
+	var onOff = true; //定义一个开关
+	$('#music').click(function(){
+		/*
+		if($('#music').hasClass('music_start')){
+			$('#music audio').get(0).pause();
+            $('#music').addClass('music_stop');
+		}else{
+			$('#music audio').get(0).play();
+            $('#music').attr('class','music_start');
+		}
+		*/
+		if ( onOff ){
+			$('#music audio').get(0).pause();
+			$('#music').addClass('music_stop').removeClass('music_start');
+			onOff = false;
+		}else{
+			$('#music audio').get(0).play();
+			$('#music').addClass('music_start').removeClass('music_stop');
+			onOff = true;
+		}
+		
+	});
+});
